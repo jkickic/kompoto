fe3da9b7948a84e1345dd6f36dcc3c114c7b9d01
zajacmateusz <jkickic+stp@gmail.com>
2016-07-18 19:57:57 +0200

    infrastructure

---
+++ infrastructure/heroku.js
@@ -0,0 +1,33 @@
+var heroin = require('heroin-js');
+
+var configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
+configurator.addPlugin(require('heroin-js-librato').plugin);
+
+var baseConf = {
+    name: 'kompoto-test',
+    region: 'eu',
+    maintenance: false,
+    stack: 'cedar-14',
+    config_vars: {PORT: '3000'},
+    addons: {
+        logentries: {plan: 'logentries:le_tryit'},
+        librato: {plan: 'librato:development'}
+    },
+    collaborators: ['mateusz.zajac@schibsted.pl'],
+    features: {
+        'runtime-dyno-metadata': {enabled: false},
+        'log-runtime-metrics': {enabled: true},
+        'http-session-affinity': {enabled: false},
+        'preboot': {enabled: false},
+        'http-shard-header': {enabled: false},
+        'http-end-to-end-continue': {enabled: false}
+    },
+    formation: [{process: 'web', quantity: 1, size: 'Free'}],
+    log_drains: []
+};
+
+var prodConf = {
+    name: 'kompoto-prod',
+};
+
+configurator(process.env.NODE_ENV === 'production' ? Object.assign(baseConf, prodConf) : baseConf);

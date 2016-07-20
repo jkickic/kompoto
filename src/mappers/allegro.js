bef30730f9de1e63596ea0f53d44ca09b35896b6
Paweł Słowik <pawel.slowik@schibsted.pl>
2016-07-20 11:37:32 +0200

    allegro mapper

--- src/mappers/allegro.js
+++ src/mappers/allegro.js
@@ -9,14 +9,16 @@ module.exports = function (item) {
         "liczba drzwi": "Liczba drzwi",
         "liczba miejsc": "Liczba miejsc",
         "nadwozie": "Typ",
-        "napęd": "Napęd"
+        "napęd": "Napęd",
+        "pojemność silnika [cm3]": "Pojemność skokowa",
+        "przebieg [km]": "Przebieg"
     };
     let params = item.params;
     Object.keys(params).forEach(key => {
         if (propertyMap[key]) {
             params.renameProperty(key, propertyMap[key]);
         }
-        params.renameProperty(key, key.toUpperCase());
+        params.renameProperty(key, key[0].toUpperCase() + key.substring(1, key.length));
     });
     return item;
 };

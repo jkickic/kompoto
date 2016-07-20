97763d0dc358290e64dd3a1bb720a43682dabfda
zajacmateusz <jkickic+stp@gmail.com>
2016-07-20 10:14:40 +0200

    allegro extractor test fixed

--- test/extractors/allegroTest.js
+++ test/extractors/allegroTest.js
@@ -15,7 +15,8 @@ describe("allegro extractor tests", function () {
                 assert.equal(ad.title, "VOLVO S60 2.0 D3 SE LUX 2010R SUMMUM 80TYŚ.KM.");
                 assert.equal(Object.keys(ad.params).length, 16);
                 assert.equal(ad.params["Moc"], "163 KM");
-                assert.equal(ad.features.length, 26);
+                assert.equal(ad.features.length, 44);
+                assert.equal(ad.img, "http://2.allegroimg.pl/s400/07/63/28/56/93/6328569330");
             });
     });
 

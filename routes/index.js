2c989499725626a63c2bb6a346d0dfea5779320b
zajacmateusz <jkickic+stp@gmail.com>
2016-07-23 12:17:51 +0200

    og

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'KOMPOTO - porównywarka ogłoszeń z otomoto, allegro, olx'});
+    res.render('index', {title: 'Kompoto - porównywarka ogłoszeń otomoto,allegro,olx'});
 });
 
 router.get('/compare', function (req, res, next) {
